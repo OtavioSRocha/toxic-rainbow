ba3ebddfd0e3a3b2d1f938cef567a0bc4b364bac
fabiodbf <fabiodbf@gmail.com>
2017-12-13 18:24:07 -0200

    Tutorial 1 - vídeo 1

--- PRBLNDR/js/main.js
+++ PRBLNDR/js/main.js
@@ -1,2 +1,50 @@
-//classe main
-alert("alert");
+//classe main - por enquanto vai estar aqui toda a bagunça seguida no tutorial
+var canvas, ctx, ALTURA, LARGURA, frames = 0;
+
+function clique(event){
+  alert("Clicou!");
+}
+
+function roda() {
+  atualiza();
+  desenha();
+
+  window.requestAnimationFrame(roda);
+}
+
+function atualiza() {
+  frames++;
+
+}
+
+function desenha() {
+  ctx.fillStyle = "#50beff";
+  ctx.fillRect(0,0,LARGURA,ALTURA);
+}
+
+function main(){
+  ALTURA = window.innerHeight;
+  LARGURA = window.innerWidth;
+
+  if (LARGURA >= 500){
+    LARGURA = 600;
+    ALTURA = 600;
+  }
+
+  canvas = document.createElement("canvas");
+  canvas.width = LARGURA;
+  canvas.height = ALTURA;
+
+  ctx = canvas.getContext("2d");
+  document.body.appendChild(canvas);
+  document.addEventListener("mousedown",clique);
+
+  roda();
+
+}
+
+
+
+
+//inicializa o jogo
+main();
